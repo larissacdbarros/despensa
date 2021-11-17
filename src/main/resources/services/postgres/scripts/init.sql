77fb83a61f42ad9942daf802606398aa01c647da
Larissa Barros <larissacarvalhoduarte@gmail.com>
2021-11-16 21:51:17 -0300

    adicionado docker-composee

---
+++ src/main/resources/services/postgres/scripts/init.sql
@@ -0,0 +1,26 @@
+create schema despensa;
+
+
+
+-- padrão de banco de dados
+--
+-- TABELAS:
+--     - começar com "tb_"
+--     - adicionar descrição da tabela
+-- CAMPOS:
+--     - ds_nome-campo: descrição
+--     - co_nome-campo: código
+--     - id_nome-campo: identificador único
+--     - st_nome-campo : boolean
+--     - tp_nome-campo: tipo
+--     OBS: Adicionar comentários aos campos
+--
+--
+-- SEQUENCE:
+--     - começar com "sq_nome_tabela"
+-- CRIAR RELACIONAMENTOS:
+--     - fk_tabela1_tabela2
+-- CONSTRAINTS:
+--     - regras exclusivas de cada tabela
+--     - começar com "ck_"
+--     - unique constranint começar "uk_"
